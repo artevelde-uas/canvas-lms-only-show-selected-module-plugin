1a90e42b6aa4e8f556426c7222ddc86a1940c851
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2018-10-23 10:53:17 +0200

    Add button to show all modules

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 
 export default function (app, options) {
     app.addRouteListener('course.modules', function () {
+        var anchors = [];
+        
         if (window.location.hash === '') return;
         
         app.addReadyListener('#context_modules', function (el) {
-            let anchors = el.querySelectorAll(`.context_module > a:not(${window.location.hash})`);
+            anchors = el.querySelectorAll(`.context_module > a:not(${window.location.hash})`);
             
             anchors.forEach(function (anchor) {
                 anchor.parentElement.classList.add('hidden');
+            });
+        });
+        
+        app.addReadyListener('#content > .header-bar', function (el) {
+            if (el.querySelector('.header-bar-left') === null) {
+                el.insertAdjacentHTML('afterbegin', `
+                    <div class="header-bar-left header-bar__module-layout">
+                        <div class="header-bar-left__buttons"></div>
+                    </div>
+                `);
+            }
+            
+            el.querySelector('.header-bar-left__buttons').insertAdjacentHTML('beforeend', `
+                <a class="btn btn-primary view_all_modules" href="#">View All Modules</a>
+            `);
+            
+            el.querySelector('.header-bar-left__buttons .view_all_modules').addEventListener('click', function () {
+                anchors.forEach(function (anchor) {
+                    anchor.parentElement.classList.remove('hidden');
+                });
             });
         });
     });
