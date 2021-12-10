d717c888dddfc96bfd8c1c5c871d05d02448e8c7
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-12-10 16:26:20 +0100

    Import styles as CSS module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { router, dom } from '@artevelde-uas/canvas-lms-app';
 import __ from './i18n';
 
-import styles from './index.css';
+import styles from './index.module.css';
 
 
 export default function () {
