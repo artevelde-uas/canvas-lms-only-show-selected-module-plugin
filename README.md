12c6299cb93aca39f64a940cc25bf4584ee0b2dd
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-12-10 16:16:36 +0100

    ﻿Use new services

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Canvas LMS Only Show Selected Module Plug-in
 
-Plugin for the [Canvas LMS theme app](https://github.com/ahsdile/canvas-lms-app) that hides other modules when linked directly to module via URL hash.
+Plugin for the [Canvas LMS theme app](https://github.com/@artevelde-uas/canvas-lms-app) that
+hides other modules when linked directly to module via URL hash.
 
 ## Installation
 
@@ -17,10 +18,10 @@ Using Yarn:
 Just import the plug-in and add it to the Canvas app:
 
 ```javascript
-import canvas from '@ahsdile/canvas-lms-app';
+import { run, addPlugin } from '@artevelde-uas/canvas-lms-app';
 import onlyShowSelectedModulePlugin from '@artevelde-uas/canvas-lms-only-show-selected-module-plugin';
 
-canvas.addPlugin(onlyShowSelectedModulePlugin);
+addPlugin(onlyShowSelectedModulePlugin);
 
-canvas.run();
+run();
 ```
--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
+import { router, dom } from '@artevelde-uas/canvas-lms-app';
 import i18n from './i18n';
 
 import styles from './index.css';
 
 
-export default function (app, options) {
-    app.addRouteListener('course.modules', function () {
+export default function () {
+    router.onRoute('courses.modules', function () {
         let moduleId = window.location.hash.replace(/^#module_/, '');
-        
+
         if (location.hash === '') return;
-        
+
         history.replaceState(null, document.title, window.location.pathname + '/' + moduleId);
 
-        app.addReadyListener('#context_modules', function (modules) {
+        dom.onElementReady('#context_modules').then(modules => {
             let selected = document.getElementById('context_module_' + moduleId);
             let intervalID = setInterval(window.scrollTo, 100, 0, 0);
-            
+
             function clear() {
                 clearInterval(intervalID);
             }
-            
+
             modules.classList.add(styles.modules);
             selected.classList.add(styles.selected);
-            
-            app.addReadyListener('#content > .header-bar', function (header) {
+
+            dom.onElementReady('#content > .header-bar').then(header => {
                 let viewAllButton;
                 let addButton = document.querySelector('.header-bar .add_module_link');
-                
+
                 addButton && addButton.classList.add('disabled');
-                
+
                 if (header.querySelector('.header-bar-left') === null) {
                     header.insertAdjacentHTML('afterbegin', `
                         <div class="header-bar-left header-bar__module-layout">
@@ -35,13 +36,13 @@ export default function (app, options) {
                         </div>
                     `);
                 }
-                
+
                 header.querySelector('.header-bar-left__buttons').insertAdjacentHTML('beforeend', `
                     <a id="view_all_modules" class="btn btn-primary">${i18n.t('view_all_modules')}</a>
                 `);
-                
+
                 viewAllButton = header.querySelector('.header-bar-left__buttons #view_all_modules')
-                
+
                 viewAllButton.addEventListener('click', function () {
                     modules.classList.remove(styles.modules);
                     history.replaceState(null, document.title, location.pathname.replace(/\/\d+$/, ''));
@@ -49,16 +50,16 @@ export default function (app, options) {
                     addButton && addButton.classList.remove('disabled');
                 });
             });
-            
+
             setTimeout(clear, 10000);
-            
+
             ['wheel', 'keydown', 'touchstart'].forEach(function (name) {
                 window.addEventListener(name, clear, {
                     capture: true,
                     once: true
                 });
             });
-            
+
             window.addEventListener('mousedown', function scrollBarHandler(e) {
                 if (e.target === document.documentElement && e.clientX >= document.documentElement.offsetWidth) {
                     window.removeEventListener('mousedown', scrollBarHandler);
