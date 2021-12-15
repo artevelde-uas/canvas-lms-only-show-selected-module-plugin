2186e8db1c42e49a323c02ab4744e1dabab4726d
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-12-15 09:20:14 +0100

    Move translations to locales folder

--- src/i18n.js
+++ src/i18n.js
@@ -1,36 +1,14 @@
 import { addTranslations, getTranslator } from '@artevelde-uas/canvas-lms-app/services/i18n';
 
 
+// Create a context with all the [lang].json file in the 'locale' directory
+const context = require.context('../locales', false, /\.json$/);
+
 // Build the translations object
-const translations = {
-    "en": {
-        "view_all_modules": "View All Modules"
-    },
-    "nl": {
-        "view_all_modules": "Alle modules weergeven"
-    },
-    "fr": {
-        "view_all_modules": "Afficher tous les modules"
-    },
-    "de": {
-        "view_all_modules": "Alle Module anzeigen"
-    },
-    "es": {
-        "view_all_modules": "Ver todas las módulos"
-    },
-    "pt": {
-        "view_all_modules": "Ver todas as módulos"
-    },
-    "it": {
-        "view_all_modules": "Visualizza tutte le moduli"
-    },
-    "zh": {
-        "view_all_modules": "查看所有单元"
-    },
-    "jp": {
-        "view_all_modules": "すべてのモジュールを表示"
-    }
-};
+const translations = Object.fromEntries(context.keys().map(key => [
+    key.replace(/^\.\/(.+)\.json$/, '$1'),
+    context(key)
+]));
 
 // Get the namespace from the package
 const namespace = require('../package.json').name;
